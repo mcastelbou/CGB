1dd69317d1cdd614e065374bc888735adb2c29ca
mcastelbou <martin.castelbou@gmail.com>
2026-04-02 11:59:32 +0200

    Début de documentation.

--- src/main/java/cgb/transfer/entity/Batch.java
+++ src/main/java/cgb/transfer/entity/Batch.java
@@ -1,6 +1,7 @@
 package cgb.transfer.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -15,24 +16,78 @@ import jakarta.persistence.OneToMany;
 @Entity
 public class Batch {
 
+	/**
+	 * L'identifiant du lot.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long batchNumber;
+
+	/**
+	 * La référence (date + numéro d'ordre) du lot.
+	 */
+	private String refBatch;
+
+	/**
+	 * Le compte source associé au lot.
+	 */
 	private String sourceAccount;
+
+	/**
+	 * La description du lot.
+	 */
 	private String description;
+
+	/**
+	 * La date du début du traitement du lot.
+	 */
 	private LocalDate startDate;
+
+	/**
+	 * Le status du lot (en cours de traitement ou traité).
+	 */
 	private String status;
 
+	/**
+	 * Lien de la liste des virements associés au lot.
+	 */
 	@JsonManagedReference
 	@OneToMany(mappedBy = "batch", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<BatchTransfer> transferList;
+	private List<BatchTransfer> transferList = new ArrayList<BatchTransfer>();
 
+	// Getters & Setters
+
+	/**
+	 * Getter de l'identifiant du lot.
+	 * 
+	 * @return L'identifiant du lot.
+	 */
 	public Long getBatchNumber() {
 		return batchNumber;
 	}
 
+	/**
+	 * Setter de l'identifiant du lot.
+	 * @param batchNumber  Le nouvel identifiant 
+	 */
 	public void setBatchNumber(Long batchNumber) {
 		this.batchNumber = batchNumber;
+	}
+
+	/**
+	 * Getter de la référence du lot.
+	 * @return  La référence du lot au format "Date-Numéro d'ordre"
+	 */
+	public String getRefBatch() {
+		return refBatch;
+	}
+
+	/**
+	 * Setter de la référence du lot.
+	 * @param refBatch  La nouvelle référence du lot
+	 */
+	public void setRefBatch(String refBatch) {
+		this.refBatch = refBatch;
 	}
 
 	public String getSourceAccount() {
